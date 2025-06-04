a10080f12fa381aa8a75a21f53c81759a52409d9
rishabhg19 <meltingbox91225@gmail.com>
2025-06-04 16:39:40 -0400

    Completed cachemanger read and write operations

--- CacheManager.swift
+++ CacheManager.swift
@@ -14,5 +14,23 @@ class CacheManager {
     
     private var cacheDir: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(cache.cacheFile)
     
+    func writeCache(movieDetails: [Int: MovieDetail]) {
+        do {
+            let data = try JSONEncoder().encode(movieDetails)
+            try data.write(to: cacheDir)
+        } catch {
+            print("Error writing to cache: \(error)")
+        }
+    }
+
+    func readCache() -> [Int: MovieDetail] {
+        do {
+            let data = try Data(contentsOf: cacheDir)
+            return try JSONDecoder().decode([Int: MovieDetail].self, from: data)
+        } catch {
+            print("Failed to read cache: \(error)")
+            return [:]
+        }
+    }
     
 }
