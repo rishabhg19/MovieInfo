c53c6a0dc4ca18d3ef0e4d512ef4489043ca2760
rishabhg19 <meltingbox91225@gmail.com>
2025-06-04 16:49:21 -0400

    Integrated caching into movie details

--- MovieDetailModel.swift
+++ MovieDetailModel.swift
@@ -9,9 +9,18 @@ import Foundation
 @MainActor
 class MovieDetailModel: ObservableObject {
     @Published var movieDetail: MovieDetail?
+    private var cache: [Int: MovieDetail] = CacheManager.cache.readCache()
 
     func fetchMovieDetail(id: Int) async {
-        movieDetail = await TMDBAPICaller().getMovieDetails(id: id)
-        print(movieDetail)
+        if let cached = cache[id] {
+            self.movieDetail = cached
+            print("USING CACHE\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+            return
+        }
+        if let apiData = await TMDBAPICaller().getMovieDetails(id: id) {
+            movieDetail = apiData
+            cache[id] = apiData
+            CacheManager.cache.writeCache(movieDetails: cache)
+        }
     }
 }
