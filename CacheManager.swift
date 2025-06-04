f39d57d98120b081439fb4c7ea78e40105d09d45
rishabhg19 <meltingbox91225@gmail.com>
2025-06-04 16:59:10 -0400

    Add support for trending movie caches

--- CacheManager.swift
+++ CacheManager.swift
@@ -14,6 +14,41 @@ class CacheManager {
     private var cacheDir: URL {
         FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(cacheFile)
     }
+    private func getTrendingMovieCacheDir(filename: String) -> URL {
+        FileManager.default
+            .urls(for: .documentDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent(filename)
+    }
+    
+    //trendingMovies_day.json and trendingMovies_week.json
+    func writeTrendingCache(_ movies: [TrendingMovie], timeWindow: String) {
+        let filename = "trendingMovies_\(timeWindow).json"
+        let url = getTrendingMovieCacheDir(filename: filename)
+        
+        do {
+            let data = try JSONEncoder().encode(movies)
+            try data.write(to: url)
+        } catch {
+            print("Failed to write trending movie cache: \(error)")
+        }
+    }
+    
+    func readTrendingCache(timeWindow: String) -> [TrendingMovie] {
+        let filename = "trendingMovies_\(timeWindow).json"
+        let url = getTrendingMovieCacheDir(filename: filename)
+        
+        guard FileManager.default.fileExists(atPath: url.path) else {
+            return []
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            return try JSONDecoder().decode([TrendingMovie].self, from: data)
+        } catch {
+            print("Error while reading trending movie cache: \(error)")
+            return []
+        }
+    }
     
     func writeCache(movieDetails: [Int: MovieDetail]) {
         do {
